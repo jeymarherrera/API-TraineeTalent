ab4245dbbd6a1d7f316c387c87ded15cb8dde2d1
jeymarherrera <jeymarherrera@gmail.com>
2023-04-25 20:20:54 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { PORT } = require("./config/config");
+const { db } = require("./config/database");
+const { server } = require("./server/index");
+
+db.authenticate()
+  .then(() => {
+    console.log("Conectado a la Base de Datos");
+    server.listen(PORT, () => {
+      console.log(`Servidor esta corriendo en el puerto ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(
+      "No se ha podido realizar la conexion a la Base de Datos:",
+      err
+    );
+  });
